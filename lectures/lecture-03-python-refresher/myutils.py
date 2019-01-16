79348185faae30ac02e32990d18cd49ad7b32ab8
Andrew Connolly <ajc@astro.washington.edu>
2019-01-16 10:39:37 -0800

    myutils

---
+++ lectures/lecture-03-python-refresher/myutils.py
@@ -0,0 +1,11 @@
+"""
+My first module with functions that I find useful.
+"""
+
+def add(a, b):
+    """ Adds two numbers, a + b """
+    return a + b
+
+def sub(a, b):
+    """ Subtracts two numbers, a - b """
+    return a - b
